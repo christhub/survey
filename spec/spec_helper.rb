176808541fafcab089d2b56fe2ae0a47e772ddae
christian lei & Evan Clough <student@epicodus.com>
2015-08-25 10:06:18 -0700

    Write spec helper file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,22 @@
 ENV['RACK_ENV'] = 'test'
+
+require 'rspec'
+require 'pg'
+require 'sinatra/activerecord'
+require 'answer'
+require 'question'
+require 'survey'
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Survey.all.each do |survey|
+      survey.destroy
+    end
+    Question.all.each do |question|
+      question.destroy
+    end
+    Answer.all.each do |answer|
+      answer.destroy
+    end
+  end
+end
