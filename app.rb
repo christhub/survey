7d2c3e1be2d0335db7e26c54fd04280e45ce3671
Evan Clough & Christian Lei <student@epicodus.com>
2015-08-25 16:35:34 -0700

    add more sinatra and questions to our survey

--- app.rb
+++ app.rb
@@ -34,18 +34,60 @@ end
 delete('/surveys/delete/:id/') do
   survey = Survey.find(params.fetch('id').to_i)
   survey.delete
-
   redirect('/surveys/')
 end
 
 get('/surveys/:id/') do
   @survey = Survey.find(params.fetch('id').to_i)
   @questions = @survey.questions
-
   erb(:survey)
 end
 
 get('/surveys/update/:id/') do
   @survey = Survey.find(params.fetch('id').to_i)
   erb(:edit_survey)
+end
+
+post('/surveys/:id/questions/') do
+  text = params.fetch('text')
+  id = params.fetch('id')
+  survey_id = params.fetch('survey_id').to_i
+  Question.create(text: text, survey_id: survey_id)
+  @survey = Survey.find(params.fetch('id').to_i)
+  @questions = @survey.questions
+  redirect("/surveys/#{survey_id}")
+end
+
+# post('/surveys/:id/answers/') do
+#   survey_id = params.fetch('id').to_i
+#   survey = Survey.find(survey_id)
+#
+#   survey.questions.each do |question|
+#     question_id = question.id
+#     answer = params.fetch("answer_to_#{question_id}")
+#     Answer.create({survey_id: survey_id, question_id: question_id, text: answer})
+#   end
+# 
+#   redirect('/surveys/')
+# end
+
+delete('/surveys/:id/question/:id/delete/') do
+  question = Question.find(params.fetch('id').to_i)
+  id = params.fetch('id')
+  question.delete
+  erb(:survey)
+end
+
+get('/survey/:id/question/:id/update/') do
+   @survey = Survey.find(params.fetch('id').to_i)
+   @question = @survey.questions
+end
+
+patch('/survey/:id/question/:id/update/') do
+  question = Question.find(params.fetch('id').to_i)
+  id = params.fetch('id')
+  survey_id = params.fetch('survey_id')
+  text = params.fetch('text')
+  Question.create(text: text, survey_id: survey_id)
+  redirect("/surveys/#{id}/questions")
 end
